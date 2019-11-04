c1c46cf5ab4541b930b4c1a0535aa8b6ed482cd3
nikaburu <nikaburu@users.noreply.github.com>
2019-11-04 22:07:17 +0000

    Migrate to netstandard2.1 and netcoreapp3.0

--- src/WpfChess.WpfPresentation/ViewModel/ViewModelLocator.cs
+++ src/WpfChess.WpfPresentation/ViewModel/ViewModelLocator.cs
@@ -13,7 +13,6 @@
 */
 
 using GalaSoft.MvvmLight.Ioc;
-using Microsoft.Practices.ServiceLocation;
 
 namespace WpfChess.WpfPresentation.ViewModel
 {
@@ -28,19 +27,6 @@ namespace WpfChess.WpfPresentation.ViewModel
         /// </summary>
         public ViewModelLocator()
         {
-            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
-
-            ////if (ViewModelBase.IsInDesignModeStatic)
-            ////{
-            ////    // Create design time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
-            ////}
-            ////else
-            ////{
-            ////    // Create run time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DataService>();
-            ////}
-
             SimpleIoc.Default.Register<MainViewModel>();
         }
 
@@ -48,7 +34,7 @@ namespace WpfChess.WpfPresentation.ViewModel
         {
             get
             {
-                return ServiceLocator.Current.GetInstance<MainViewModel>();
+                return SimpleIoc.Default.GetInstance<MainViewModel>();
             }
         }
         
